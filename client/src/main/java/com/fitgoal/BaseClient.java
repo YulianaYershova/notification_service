1c2a0b437c7c1073d07f147f021a7bafc21d04cf
Yuliana <juliana.ershova97@gmail.com>
2019-11-11 14:48:25 +0200

    Modified htttpClient

--- client/src/main/java/com/fitgoal/BaseClient.java
+++ client/src/main/java/com/fitgoal/BaseClient.java
@@ -3,6 +3,7 @@ package com.fitgoal;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.dropwizard.jackson.Jackson;
+import okhttp3.HttpUrl;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -12,25 +13,34 @@ import okhttp3.ResponseBody;
 
 import javax.inject.Inject;
 import java.io.IOException;
+import java.util.Arrays;
+
+import static java.net.HttpURLConnection.*;
 
 abstract class BaseClient {
 
     private static final MediaType JSON_MEDIA_TYPE = MediaType.parse("application/json; charset=utf-8");
-    private final ObjectMapper mapper;
+    private static final int UNPROCESSABLE_ENTITY_STATUS_CODE = 422;
     private final OkHttpClient okHttpClient;
+    private final ObjectMapper mapper;
+    private final HttpUrl resourceUrl;
 
     @Inject
-    BaseClient(ObjectMapper mapper, OkHttpClient okHttpClient) {
-        this.mapper = mapper;
+    BaseClient(OkHttpClient okHttpClient, ObjectMapper mapper, HttpUrl resourceUrl) {
         this.okHttpClient = okHttpClient;
-    }
-
-    RequestBody fromEntity(Object entity) {
-        return RequestBody.create(writeValueAsString(entity), JSON_MEDIA_TYPE);
+        this.mapper = mapper;
+        this.resourceUrl = resourceUrl;
     }
 
     static ObjectMapper buildDefaultMapper() {
         return Jackson.newObjectMapper();
+    }
+
+    /**
+     * Build {@link RequestBody} converting object to JSON
+     */
+    RequestBody fromEntity(Object entity) {
+        return RequestBody.create(writeValueAsString(entity), JSON_MEDIA_TYPE);
     }
 
     private String writeValueAsString(Object object) {
@@ -42,15 +52,32 @@ abstract class BaseClient {
     }
 
     void executeRequest(Request request) {
-        try {
-            okHttpClient.newCall(request).execute();
+        try (Response response = okHttpClient.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                throw convertException(response);
+            }
         } catch (IOException e) {
             throw new RuntimeException(e.getMessage());
         }
-        /*try (Response response = okHttpClient.newCall(request).execute()) {
-            return response.code();
-        } catch (IOException e) {
-            throw new RuntimeException(e.getMessage());
-        }*/
+    }
+
+    private RuntimeException convertException(Response unsuccessfulResponse) throws IOException {
+        try (Response response = unsuccessfulResponse) {
+            ResponseBody responseBody = response.body();
+            String message = responseBody != null ? responseBody.string() : null;
+            switch (response.code()) {
+                case HTTP_BAD_REQUEST:
+                case UNPROCESSABLE_ENTITY_STATUS_CODE:
+                    return new IllegalArgumentException(message);
+                default:
+                    return new RuntimeException(message);
+            }
+        }
+    }
+
+    HttpUrl buildSubresourceHttpUrl(String... subresourcePaths) {
+        HttpUrl.Builder builder = resourceUrl.newBuilder();
+        Arrays.stream(subresourcePaths).forEach(builder::addPathSegment);
+        return builder.build();
     }
 }
