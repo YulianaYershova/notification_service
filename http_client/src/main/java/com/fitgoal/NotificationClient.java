3c124b0021d6124972b97fa7bed7feecea2e19e3
Yuliana <juliana.ershova97@gmail.com>
2019-11-13 12:48:50 +0200

    Provided okhttp client

---
+++ http_client/src/main/java/com/fitgoal/NotificationClient.java
@@ -0,0 +1,65 @@
+package com.fitgoal;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fitgoal.api.NotificationService;
+import com.fitgoal.api.domain.Recipient;
+import com.fitgoal.api.domain.UserVerification;
+import lombok.NonNull;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * OkHttp HTTP client implementation of {@link NotificationService} API.
+ */
+public class NotificationClient extends BaseClient implements NotificationService {
+
+    private static final String REGISTER = "register";
+    private static final String RESET_PASSWORD = "resetPassword";
+    private static final String SUCCESS = "success";
+
+    public NotificationClient(OkHttpClient okHttpClient, HttpUrl resourceUrl) {
+        this(okHttpClient, buildDefaultMapper(), resourceUrl);
+    }
+
+    public NotificationClient(@NonNull OkHttpClient okHttpClient, @NonNull ObjectMapper mapper, @NonNull HttpUrl resourceUrl) {
+        super(okHttpClient, mapper, resourceUrl);
+    }
+
+    @Override
+    public void register(UserVerification userVerification) {
+        HttpUrl httpUrl = buildSubresourceHttpUrl(REGISTER);
+        Request request = buildRequest(userVerification, httpUrl);
+        executeRequest(request);
+    }
+
+    @Override
+    public void registerSuccess(Recipient recipient) {
+        HttpUrl httpUrl = buildSubresourceHttpUrl(REGISTER, SUCCESS);
+        Request request = buildRequest(recipient,httpUrl);
+        executeRequest(request);
+    }
+
+    @Override
+    public void resetPassword(UserVerification userVerification) {
+        HttpUrl httpUrl = buildSubresourceHttpUrl(RESET_PASSWORD);
+        Request request = buildRequest(userVerification, httpUrl);
+        executeRequest(request);
+    }
+
+    @Override
+    public void resetPasswordSuccess(Recipient recipient) {
+        HttpUrl httpUrl = buildSubresourceHttpUrl(RESET_PASSWORD, SUCCESS);
+        Request request = buildRequest(recipient,httpUrl);
+        executeRequest(request);
+    }
+
+    @NotNull
+    private Request buildRequest(Object value, HttpUrl httpUrl) {
+        return new Request.Builder()
+                .url(httpUrl)
+                .post(fromEntity(value))
+                .build();
+    }
+}
