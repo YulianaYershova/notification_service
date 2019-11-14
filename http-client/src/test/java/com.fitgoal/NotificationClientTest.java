a6af208ec73f9e95fbe240a49c624a3140c85679
Yuliana <juliana.ershova97@gmail.com>
2019-11-14 13:13:27 +0200

    Provided tests for okhttp client

--- http-client/src/test/java/com.fitgoal/NotificationClientTest.java
+++ http-client/src/test/java/com.fitgoal/NotificationClientTest.java
@@ -1,5 +1,6 @@
 package com.fitgoal;
 
+import com.fitgoal.api.domain.Recipient;
 import com.fitgoal.api.domain.UserVerification;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
@@ -10,9 +11,11 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import javax.ws.rs.HttpMethod;
-import java.io.IOException;
+
+import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.eclipse.jetty.http.HttpStatus.NO_CONTENT_204;
 
 public class NotificationClientTest {
 
@@ -26,31 +29,67 @@ public class NotificationClientTest {
     private NotificationClient notificationClient = new NotificationClient(new OkHttpClient(), resourceRoot);
 
     @Test
-    public void register() throws InterruptedException, IOException {
+    public void register() throws InterruptedException {
         UserVerification userVerification = new UserVerification("test@test.com", "link");
 
-        mockWebServer.enqueue(new MockResponse().setResponseCode(204));
+        mockWebServer.enqueue(new MockResponse().setResponseCode(NO_CONTENT_204));
+
         notificationClient.register(userVerification);
-        // verify the request was sent
         RecordedRequest recordedRequest = mockWebServer.takeRequest();
+
+        HttpUrl httpUrl = buildSubresourceHttpUrl(REGISTER);
+
+        assertThat(recordedRequest.getRequestUrl()).isEqualTo(httpUrl);
         assertThat(recordedRequest.getMethod()).isEqualTo(HttpMethod.POST);
     }
 
     @Test
     public void registerSuccess() throws InterruptedException {
+        Recipient recipient = new Recipient("test@test.com");
 
+        mockWebServer.enqueue(new MockResponse().setResponseCode(NO_CONTENT_204));
+
+        notificationClient.registerSuccess(recipient);
+        RecordedRequest recordedRequest = mockWebServer.takeRequest();
+
+        HttpUrl httpUrl = buildSubresourceHttpUrl(REGISTER, SUCCESS);
+
+        assertThat(recordedRequest.getRequestUrl()).isEqualTo(httpUrl);
+        assertThat(recordedRequest.getMethod()).isEqualTo(HttpMethod.POST);
     }
 
     @Test
-    public void resetPassword() {
+    public void resetPassword() throws InterruptedException {
+        UserVerification userVerification = new UserVerification("test@test.com", "link");
+
+        mockWebServer.enqueue(new MockResponse().setResponseCode(NO_CONTENT_204));
+
+        notificationClient.resetPassword(userVerification);
+        RecordedRequest recordedRequest = mockWebServer.takeRequest();
+
+        HttpUrl httpUrl = buildSubresourceHttpUrl(RESET_PASSWORD);
+
+        assertThat(recordedRequest.getRequestUrl()).isEqualTo(httpUrl);
+        assertThat(recordedRequest.getMethod()).isEqualTo(HttpMethod.POST);
     }
 
     @Test
-    public void resetPasswordSuccess() {
+    public void resetPasswordSuccess() throws InterruptedException {
+        Recipient recipient = new Recipient("test@test.com");
+
+        mockWebServer.enqueue(new MockResponse().setResponseCode(NO_CONTENT_204));
+
+        notificationClient.resetPasswordSuccess(recipient);
+        RecordedRequest recordedRequest = mockWebServer.takeRequest();
+        HttpUrl httpUrl = buildSubresourceHttpUrl(RESET_PASSWORD, SUCCESS);
+
+        assertThat(recordedRequest.getRequestUrl()).isEqualTo(httpUrl);
+        assertThat(recordedRequest.getMethod()).isEqualTo(HttpMethod.POST);
     }
-  /*  private HttpUrl buildSubresourceHttpUrl(String... subresourcePaths) {
+
+    private HttpUrl buildSubresourceHttpUrl(String... subresourcePaths) {
         HttpUrl.Builder builder = resourceRoot.newBuilder();
         Arrays.stream(subresourcePaths).forEach(builder::addPathSegment);
         return builder.build();
-    }*/
+    }
 }
